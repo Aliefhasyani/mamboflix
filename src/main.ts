6f7c7408d2494f5fd820eeb172f6aa5ccbd326ad
Aliefhasyani <aliefhasyani05@gmail.com>
2026-05-05 17:50:36 +0800

    added vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import Home from './pages/Home.vue'
 import './assets/main.css' 
+import { Analytics } from "@vercel/analytics/next"
 
 createApp(Home).mount('#app')
